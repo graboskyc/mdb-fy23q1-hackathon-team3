20bfad8f86cf5610743922ad53b0c8cf6bc6bdd4
Josh Edward Smith <josh.smith@Joshs-MacBook-Pro.local>
2022-03-14 15:42:35 -0500

    Adding componet for form detail

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormDetailComponent } from './form-detail/form-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/form-detail/form-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-detail',
+  templateUrl: './form-detail.component.html',
+  styleUrls: ['./form-detail.component.sass']
+})
+export class FormDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
