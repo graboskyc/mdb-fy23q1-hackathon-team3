17c98517519df529f6db515ef79b3427f6a83eb9
Chris Grabosky <chris@grabosky.net>
2022-03-14 17:22:18 -0400

    actual login working and skeleton

---
+++ FlexFormMobile/FlexFormMobile/FlexFormMobile/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Realms;
+
+namespace FlexFormMobile
+{
+    public partial class App : Application
+    {
+
+        public static string realm_Username { get; set; }
+        public static Realms.Sync.App realm_app { get; set; }
+        public static Realms.Sync.User realm_user { get; set; }
+        public static Realms.Sync.SyncConfiguration realm_config { get; set; }
+        public static Realm realm_realm { get; set; }
+        public static string realm_partition { get; set; }
+        public static bool realm_IsLoggedIn { get; set; } = false;
+        public static bool realm_LoginFailed { get; set; } = false;
+        public static string realm_Gravatar { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnStart()
+        {
+            MainPage = new NavigationPage(new LoginPage());
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FlexFormMobile/FlexFormMobile/FlexFormMobile/LoginPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Realms;
+using Realms.Sync;
+
+namespace FlexFormMobile
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_login_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                App.realm_Username = txt_email.Text;
+
+                using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(txt_email.Text);
+                    byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sb.Append(hashBytes[i].ToString("X2"));
+                    }
+                    App.realm_Gravatar = sb.ToString().ToLower();
+                }
+
+                App.realm_app = Realms.Sync.App.Create("flexform-ggkwy");
+                App.realm_user = await App.realm_app.LogInAsync(Realms.Sync.Credentials.EmailPassword(txt_email.Text, txt_password.Text));
+                App.realm_IsLoggedIn = true;
+                App.realm_LoginFailed = false;
+                await Navigation.PushAsync(new TabbedMenu());
+            }
+            catch (Exception ex)
+            {
+                App.realm_IsLoggedIn = false;
+                App.realm_LoginFailed = true;
+                await DisplayAlert("Login Failed", ex.Message, "OK");
+            }
+        }
+    }
+}
